bfa9c47cac23a2403981fcbd617b800326451df6
Andy Wood <mongolsamurai@gmail.com>
2016-06-16 21:21:08 -0700

    Add new VisibleEvent middleware class to handle message display

--- muss/scripts.py
+++ muss/scripts.py
@@ -1,107 +1,73 @@
 from muss import db, locks
 
-class CreateObject(db.Event):
+class CreateObject(db.VisibleEvent):
     """
     Class for scripted events which cause the creation of a new object
     """
-    def __init__(self, name, emit, send=None, owner=None, disruptive=False):
-        with locks.authority_of(locks.SYSTEM):
-            self.emit_string = emit
-            self.send_string = send
-            self.disruptive = disruptive
-        super(CreateObject, self).__init__(name, owner=owner, disruptive=disruptive)
+    def __init__(self, name, emit=None, send=None, owner=None,
+            disruptive=False, exempt=None):
+        super(CreateObject, self).__init__(name, emit=emit,
+                send=send, owner=owner, disruptive=disruptive, exempt=exempt)
 
     def trigger(self, source, player, **kwargs):
-        try:
-           player.send(self.send_string.format(obj=source.name))
-           source.emit(self.emit_string.format(obj=source.name, player=player.name), exceptions=[player])
-        except AttributeError:
-            source.emit(self.emit_string.format(obj=source.name, player=player.name))
         # TODO: Implement appropriate changes to world database
         super(CreateObject, self).trigger()
 
-class SelfDestruct(db.Event):
+class SelfDestruct(db.VisibleEvent):
     """
     Class for scripted events which destroy the object to which they are registered
     """
-    def __init__(self, name, emit, send=None, owner=None, exempt=None):
-        super(SelfDestruct, self).__init__(name, owner=owner, disruptive=True, exempt=None)
-        with locks.authority_of(locks.SYSTEM):
-            self.emit_string = emit
-            self.send_string = send
+    def __init__(self, name, emit=None, send=None, owner=None, exempt=None):
+        super(SelfDestruct, self).__init__(name, emit=emit,
+                send=send, owner=owner, disruptive=True, exempt=exempt)
 
     def trigger(self, source, player, **kwargs):
-        try:
-           player.send(self.send_string.format(obj=source.name))
-           source.emit(self.emit_string.format(obj=source.name, player=player.name),
-                   exceptions=[player])
-        except AttributeError:
-            source.emit(self.emit_string.format(obj=source.name, player=player.name))
         with locks.authority_of(locks.SYSTEM):
             source.destroy()
-        super(SelfDestruct, self).trigger()
+        super(SelfDestruct, self).trigger(source, player)
 
-class ApplyAttribute(db.Event):
+class ApplyAttribute(db.VisibleEvent):
     """
     Class for scripted events which apply a custom attribute to an object
     """
-    def __init__(self, name, emit, send=None, owner=None, disruptive=False):
-        with locks.authority_of(locks.SYSTEM):
-            self.emit_string = emit
-            self.send_string = send
-            self.disruptive = disruptive
-        super(ApplyAttribute, self).__init__(name, owner=owner, disruptive=disruptive)
+    def __init__(self, name, emit=None, send=None, owner=None,
+            disruptive=False, exempt=None):
+        super(ApplyAttribute, self).__init__(name, emit=emit,
+                send=send, owner=owner, disruptive=disruptive, exempt=exempt)
 
     def trigger(self, source, player, **kwargs):
-        try:
-           player.send(self.send_string.format(obj=source.name))
-           source.emit(self.emit_string.format(obj=source.name, player=player.name), exceptions=[player])
-        except AttributeError:
-            source.emit(self.emit_string.format(obj=source.name, player=player.name))
         # TODO: Implement appropriate changes to world database
         super(ApplyAttribute, self).trigger()
 
-class ChangeAttribute(db.Event):
+class ChangeAttribute(db.VisibleEvent):
     """
     Class for scripted events which modify a custom attribute on an object
     """
-    def __init__(self, name, emit, send=None, owner=None, disruptive=False):
-        with locks.authority_of(locks.SYSTEM):
-            self.emit_string = emit
-            self.send_string = send
-            self.disruptive = disruptive
-        super(ChangeAttribute, self).__init__(name, owner=owner, disruptive=disruptive)
+    def __init__(self, name, emit=None, send=None, owner=None,
+            disruptive=False, exempt=None):
+        super(ChangeAttribute, self).__init__(name, emit=emit,
+                send=send, owner=owner, disruptive=disruptive, exempt=exempt)
 
     def trigger(self, source, player, **kwargs):
-        try:
-           player.send(self.send_string.format(obj=source.name))
-           source.emit(self.emit_string.format(obj=source.name, player=player.name), exceptions=[player])
-        except AttributeError:
-            source.emit(self.emit_string.format(obj=source.name, player=player.name))
         # TODO: Implement appropriate changes to world database
         super(ChangeAttribute, self).trigger()
 
-class RemoveAttribute(db.Event):
+class RemoveAttribute(db.VisibleEvent):
     """
     Class for scripted events which remove a custom attribute from an object
     """
-    def __init__(self, name, emit, send=None, owner=None, disruptive=False):
-        with locks.authority_of(locks.SYSTEM):
-            self.emit_string = emit
-            self.send_string = send
-            self.disruptive = disruptive
-        super(RemoveAttribute, self).__init__(name, owner=owner, disruptive=disruptive)
+    def __init__(self, name, emit=None, send=None, owner=None,
+            disruptive=False, exempt=None):
+        super(RemoveAttribute, self).__init__(name, emit=emit,
+                send=send, owner=owner, disruptive=disruptive, exempt=exempt)
 
     def trigger(self, source, player, **kwargs):
-        try:
-           player.send(self.send_string.format(obj=source.name))
-           source.emit(self.emit_string.format(obj=source.name, player=player.name), exceptions=[player])
-        except AttributeError:
-            source.emit(self.emit_string.format(obj=source.name, player=player.name))
         # TODO: Implement appropriate changes to world database
         super(RemoveAttribute, self).trigger()
 
-disintegrate = SelfDestruct('disintegrate', 'The {obj} disintegrates!')
-explode = SelfDestruct('explode', 'The {obj} explodes violently!')
-absorb = SelfDestruct('absorb', 'The {obj} glows briefly, and absorbs into {player}',
+disintegrate = SelfDestruct('disintegrate', emit='The {obj} disintegrates!')
+explode = SelfDestruct('explode', emit='The {obj} explodes violently!')
+absorb = SelfDestruct('absorb',
+        emit='The {obj} glows briefly, and absorbs into {player}',
         send='The {obj} glows bright, and absorbs into you!')
+disruptive_curse = db.VisibleEvent('disrupt', send='The {obj} is cursed!', disruptive=True)
